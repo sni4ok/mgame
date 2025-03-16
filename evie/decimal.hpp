f9cf7c6897be840f5b5e8107670a6a6c19b79040
sni4ok <sni4ok@yandex.ru>
2025-03-16 03:03:30 +0300

    fixes

---
+++ evie/decimal.hpp
@@ -0,0 +1,95 @@
+#pragma once
+
+#ifndef DECIMAL_HPP
+#define DECIMAL_HPP
+
+#include "type_traits.hpp"
+
+template<typename type>
+concept is_decimal = is_class_v<type> && requires(type* t)
+{
+    t->frac;
+    t->exponent;
+};
+
+template<typename type>
+requires(is_decimal<type>)
+constexpr inline bool operator<(type l, type r)
+{
+    return l.value < r.value;
+}
+
+template<typename type>
+requires(is_decimal<type>)
+constexpr inline bool operator>(type l, type r)
+{
+    return l.value > r.value;
+}
+
+template<typename type>
+requires(is_decimal<type>)
+constexpr inline bool operator!=(type l, type r)
+{
+    return l.value != r.value;
+}
+
+template<typename type>
+requires(is_decimal<type>)
+constexpr inline bool operator==(type l, type r)
+{
+    return l.value == r.value;
+}
+
+template<typename type>
+requires(is_decimal<type>)
+constexpr inline bool operator<=(type l, type r)
+{
+    return l.value <= r.value;
+}
+
+template<typename type>
+requires(is_decimal<type>)
+constexpr inline bool operator>=(type l, type r)
+{
+    return l.value >= r.value;
+}
+
+template<typename type>
+requires(is_decimal<type>)
+constexpr inline type operator+(type l, type r)
+{
+    return {l.value + r.value};
+}
+
+template<typename type>
+requires(is_decimal<type>)
+constexpr inline type operator-(type l, type r)
+{
+    return {l.value - r.value};
+}
+
+template<typename type>
+requires(is_decimal<type>)
+constexpr inline type& operator+=(type& l, type r)
+{
+    l.value += r.value;
+    return l;
+}
+
+template<typename type>
+requires(is_decimal<type>)
+constexpr inline type& operator-=(type& l, type r)
+{
+    l.value -= r.value;
+    return l;
+}
+
+template<typename type>
+requires(is_decimal<type>)
+bool operator!(type v)
+{
+    return !v.value;
+}
+
+#endif
+
